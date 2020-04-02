87cc8a6f3377d56a17dfb2ea926fae5e28457afa
vukimduy <duyvukim752000@gmail.com>
2020-04-02 23:39:36 +1100

    added remaining chances and number of limit chances

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ export default class Game extends Component {
   state = {
     secret : 0,
     input: '',
-    feedback: ''
+    feedback: '',
+    numOfGuess: 10
   }
 
   //function to pick a random number
@@ -25,6 +26,7 @@ export default class Game extends Component {
   //function to initialize the game
   init() {
     const secretNumber = this.generateRandom()
+    this.state.numOfGuess = 10
     this.setState({ secret: secretNumber })
   }
 
@@ -32,11 +34,26 @@ export default class Game extends Component {
   componentDidMount() {
     this.init()
   }
+  
+  componentWillUnmount()
+  {
+    this.init()
+  }
+  
 
   //update input state
   updateInput = (value) => {
     this.setState({input: value})
-    
+  }
+
+  //update changes
+  updateChange() {
+    this.state.numOfGuess--
+    if(this.state.numOfGuess == 0)
+    {
+      this.setState({feedback: "You are the catchy loser!!"})
+      this.init()
+    }
   }
 
   checkGuess = () => {
@@ -53,19 +70,21 @@ export default class Game extends Component {
     if ( userGuess < secretNumber)
     {
       this.setState({ feedback: 'You guessed smaller than the number!'})
+      this.updateChange()
     }
 
     if ( userGuess > secretNumber)
     {
       this.setState({ feedback: 'You guessed larger than the number!'})
+      this.updateChange()
     }
     return
   }
 
   render () {
     return(
-      <View style={styles.container}>
-        <Text>Guess my number,WOWOWOW</Text>
+      <View style={styles.game}>
+        <Text style = {styles.title}>Guess my number</Text>
         
         <TextInput 
         style = {styles.input} 
@@ -79,14 +98,23 @@ export default class Game extends Component {
         onPress={this.checkGuess}>
           <Text>Submit Guess</Text>
         </TouchableHighlight>
-        <Text>{this.state.feedback}</Text>
-      
+
+        <Text
+        style = {styles.feedback}>{this.state.feedback}</Text>
+
+        <Text>{"Remaining chances: " + this.state.numOfGuess}</Text>
+
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  feedback: {
+    color: '#ffffff',
+    fontSize: 14,
+    marginTop: 20
+  },
 
   button: {
     width: 200,
@@ -96,21 +124,30 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
 
-  container: {
+  game: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#660066',
+    padding: 40,
+    borderRadius: 50,
+    minHeight: 300,
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   text: {
     color: '#ff33ff',
-    fontSize: 32
+    fontSize: 50
+  },
+
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: '#fcfcbd'
   },
 
   input: {
     backgroundColor: '#ffffff',
-    width: 100,
+    width: 200,
     marginTop: 10,
     padding: 10,
     textAlign: 'center'
